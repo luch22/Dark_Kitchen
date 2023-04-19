a12c7e083587905008ca30a58bfb569b72846902
Bruno <brunolb.lafont@gmail.com>
2023-04-19 15:50:11 +0200

    add cart

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 function filter(listMenu, tags) {
-  var monTableau = new Array();
+  let monTableau = new Array();
   listMenu.forEach((menu) => {
     menu.forEach((plat) => {
       plat.classes.forEach((element) => {
@@ -723,7 +723,69 @@ elem.forEach((object) => {
 	ingredients.textContent = "ingrédients : " + object.ingredients;
 	cardBody.appendChild(ingredients);
 
-
 	main.appendChild(card);
+
 });
 });
+
+
+// Define an object with items
+const items = [
+    { id: 1, name: 'Item 1', price: 10 },
+    { id: 2, name: 'Item 2', price: 20 },
+    { id: 3, name: 'Item 3', price: 30 },
+  ];
+  
+  // Get the cart element from the HTML
+  const cart = document.getElementById('cart');
+  
+  // Initialize the cart state
+  let cartItems = [];
+  
+  // Add an item to the cart
+  function addToCart(itemId) {
+    const item = items.find(item => item.id === itemId);
+    if (item) {
+      cartItems.push(item);
+      updateCart();
+    }
+  }
+  
+  // Remove an item from the cart
+  function removeFromCart(index) {
+    cartItems.splice(index, 1);
+    updateCart();
+  }
+  
+  // Update the cart interface
+  function updateCart() {
+    const cartItemsElement = document.getElementById('cart-items');
+    cartItemsElement.innerHTML = '';
+  
+    for (let i = 0; i < cartItems.length; i++) {
+      const item = cartItems[i];
+      const listItem = document.createElement('li');
+      listItem.innerHTML = `${item.name} - $${item.price}`;
+      const removeButton = document.createElement('button');
+      removeButton.innerHTML = 'Remove';
+      removeButton.addEventListener('click', () => removeFromCart(i));
+      listItem.appendChild(removeButton);
+      cartItemsElement.appendChild(listItem);
+    }
+  }
+  
+  // Listen for clicks on the Add to Cart buttons
+  const addToCartButtons = document.querySelectorAll('.add-to-cart');
+  addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const itemId = parseInt(button.dataset.itemId);
+      addToCart(itemId);
+    });
+  });
+  
+  // Listen for clicks on the checkout button
+  const checkoutButton = document.getElementById('checkout');
+  checkoutButton.addEventListener('click', () => {
+    // Implement the checkout process here
+  });
+  
