d3a2582faf670a57aa97fed83ede672490f41975
Luciano <Luciano.cia3@gmail.com>
2023-04-19 15:46:30 +0200

    "Organisation function affichage"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,60 @@
 import { fullMenu } from "./objetMenu.js";
+//#region Function
+function affichageHtml(listMenu) {
+  listMenu.forEach((menu) => {
+    let catmenu = menu[0].categorie[0];
+    let sectionParent = document.createElement("section");
+    sectionParent.setAttribute("class", catmenu);
+    sectionParent.innerHTML = `<h3>${catmenu}</h3>  `;
+    menu.forEach((plat) => {
+      sectionParent.append(listerPlat(plat));
+      main.append(sectionParent);
+    });
+  });
+}
 
-//#region filtre et tri
+function listerPlat(plat) {
+  const card = document.createElement("div");
+  card.setAttribute("class", "card");
+  plat.categorie.forEach((e) => {
+    card.classList.add(e);
+  });
+
+  let picture = document.createElement("img");
+  picture.classList.add("img");
+  picture.src = plat.picture;
+  card.appendChild(picture);
+
+  let cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+  card.appendChild(cardBody);
+
+  let name = document.createElement("h2");
+  name.classList.add("h2");
+  name.textContent = plat.name;
+  cardBody.appendChild(name);
+
+  let prix = document.createElement("span");
+  prix.classList.add("item");
+  prix.textContent = "Prix : " + plat.prix;
+  cardBody.appendChild(prix);
+
+  let categorie = document.createElement("span");
+  categorie.classList.add("item");
+  categorie.textContent = "Catégories : " + plat.categorie;
+  cardBody.appendChild(categorie);
+
+  let ingredients = document.createElement("span");
+  ingredients.classList.add("item");
+  ingredients.textContent = "ingrédients : " + plat.ingredients;
+  cardBody.appendChild(ingredients);
+  return card;
+}
 function filterMenu(listMenu, tags) {
-  var monTableau = new Array();
+  let monTableau = new Array();
   listMenu.forEach((menu) => {
     menu.forEach((plat) => {
-      plat.classes.forEach((element) => {
+      plat.categorie.forEach((element) => {
         if (tags.includes(element)) {
           monTableau.push(plat);
         }
@@ -16,7 +65,7 @@ function filterMenu(listMenu, tags) {
 }
 
 function sortMenu(listMenu) {
-  var monTableau = new Array();
+  let monTableau = new Array();
   listMenu.forEach((menu) => {
     menu.forEach((plat) => {
       monTableau.push(plat);
@@ -28,8 +77,6 @@ function sortMenu(listMenu) {
   });
   return monTableau;
 }
-// console.log(filterMenu(fullMenu, "maki"));
-// console.log(sortMenu(fullMenu));
 //#endregion
 
 const bouton = document.body.querySelector("#darkmode");
@@ -39,47 +86,11 @@ bouton.addEventListener("click", () => {
   body.classList.toggle("dark-mode");
 });
 
+fullMenu.forEach((menu) => {
+  console.log(menu[0].categorie[0]);
+});
+
 console.log(bouton.getAttribute("type"));
 
 const main = document.querySelector("main");
-
-fullMenu.forEach((elem) => {
-  elem.forEach((object) => {
-    const card = document.createElement("div");
-    card.setAttribute("class", "card");
-    object.classes.forEach((e) => {
-      card.classList.add(e);
-    });
-
-    let picture = document.createElement("img");
-    picture.classList.add("img");
-    picture.src = object.picture;
-    card.appendChild(picture);
-
-    let cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
-    card.appendChild(cardBody);
-
-    let name = document.createElement("h2");
-    name.classList.add("h2");
-    name.textContent = object.name;
-    cardBody.appendChild(name);
-
-    let prix = document.createElement("span");
-    prix.classList.add("item");
-    prix.textContent = "Prix : " + object.prix;
-    cardBody.appendChild(prix);
-
-    let classes = document.createElement("span");
-    classes.classList.add("item");
-    classes.textContent = "Catégories : " + object.classes;
-    cardBody.appendChild(classes);
-
-    let ingredients = document.createElement("span");
-    ingredients.classList.add("item");
-    ingredients.textContent = "ingrédients : " + object.ingredients;
-    cardBody.appendChild(ingredients);
-
-    main.appendChild(card);
-  });
-});
+affichageHtml(fullMenu);
